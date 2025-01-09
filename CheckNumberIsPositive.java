920964e72bfc73caf92c3e7a619c82a97a05d339
ArunNatarajankalyan <130858473+ArunNatarajanKalyan@users.noreply.github.com>
2025-01-10 00:44:54 +0530

    Add files via upload
    Week1 Assignment

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number=108;
+		if(number >0) {
+			System.out.println("Positive Number");	
+		}
+		else if (number<0) {
+			System.out.println("Negative Number");
+		}
+	}
+
+}
---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2009;
+		char shortcutkey='C';
+		System.out.println("Version is:"+version);
+		System.out.println("Developer is:"+developer);
+		System.out.println("beta is:"+isBeta);
+		System.out.println("releaseYear is:"+releaseYear);
+		System.out.println("shortcutkey is:"+shortcutkey);
+	}
+
+}
